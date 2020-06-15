c30f9b8b5781243898b663ace1469791e1be822e
Varun Maliwal <dev@varun.io>
2020-06-15 14:03:19 +0530

    Added required plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,48 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.md`, `.mdx`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `inter`,
+          `source sans pro\:300,400,500i,600,700`,
+          `zilla\:300,400,500i,600,700`,
+          `domine`,
+        ],
+        display: "swap",
+      },
+    },
+  ],
 }
