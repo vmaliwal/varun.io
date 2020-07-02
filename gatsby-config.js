5a2e1b787059edf5015cb0f495ff889fcf39175c
Varun Maliwal <dev@varun.io>
2020-07-02 18:25:13 +0530

    Added g analytics support

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,5 +53,16 @@ module.exports = {
         display: 'swap',
       },
     },
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: `UA-171545803-1`,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
+      },
+    },
   ],
 };
